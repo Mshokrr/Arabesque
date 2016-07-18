fb6a54f6fc5951b8d8b3e6c7bd7606edeb3dfa2f
Mshokrr <mohamedshokrr@gmail.com>
2016-07-18 17:50:52 +0200

    completed gallery: gallery.html, gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -4,44 +4,44 @@ $('document').ready(function(){
 });
 
 $('div#galleryLink1').click(function(){
-	$('h2.gallery-heading').text('gallery1');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 1');
+	$('div#gallery1').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
 $('div#galleryLink2').click(function(){
-	$('h2.gallery-heading').text('gallery2');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 2');
+	$('div#gallery2').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
 $('div#galleryLink3').click(function(){
-	$('h2.gallery-heading').text('gallery3');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 3');
+	$('div#gallery3').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
 $('div#galleryLink4').click(function(){
-	$('h2.gallery-heading').text('gallery4');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 4');
+	$('div#gallery4').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
 $('div#galleryLink5').click(function(){
-	$('h2.gallery-heading').text('gallery5');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 5');
+	$('div#gallery5').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
 $('div#galleryLink6').click(function(){
-	$('h2.gallery-heading').text('gallery6');
-	$('div#gallery1').show();
-	$('div#galleryBackBtn').show();
+	$('h2.gallery-heading').text('Gallery 6');
+	$('div#gallery6').fadeIn();
+	$('div#galleryBackBtn').fadeIn();
 	$('div#mainGallery').hide();
 });
 
@@ -49,5 +49,5 @@ $('btn#galleryBackBtn').click(function(){
 	$('h2.gallery-heading').text('Gallery');
 	$('div.gallery-content').hide();
 	$('div#galleryBackBtn').hide();
-	$('div#mainGallery').show();
+	$('div#mainGallery').fadeIn();
 });
