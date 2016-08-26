aa406502a596d8a22d54fca1da76b6f094840c1d
Youssef <youssefswailem@gmail.com>
2016-08-27 00:16:10 +0200

    Updated error messages. See comments inside

--- app/config/passport.js
+++ app/config/passport.js
@@ -13,12 +13,19 @@ passport.use(new localStrategy({
 		}
 		if(!user) {
 			return done(null, false, {
-			message: 'User was not found'
+			message: 'Incorrect credentials' ///***
 				});
 			}
 		if(!user.validPassword(password)) {
 			return done(null, false, {
-				message: 'Incorrect Password'
+				message: 'Incorrect credentials'  //****
+
+				//**
+				//never tell the user which his credentials is incorrect this makes gaining access
+				//to the account easier because now a hacker knows that one of the fields is correct
+				//you should only  mention that one of them is not correct without specifying which
+
+
 			});
 		}
 		return done(null, user);
