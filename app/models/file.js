b376771850bee524205e5953062e8c7cac88e995
Youssef Swailem <youssefswailem@gmail.com>
2016-08-30 18:43:28 +0200

    Fixed formatting issue and added file model

---
+++ app/models/file.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var fileSchema = new mongoose.Schema({
+
+    uploaderID: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    uploaderName: {type: String, required: true},
+
+    file: {
+        filename: String,
+        filepath: String,   //path to the file on the server (after upload)
+        size: Number
+    },
+
+    description: String,
+    dateUploaded: String,
+
+});
+
+mongoose.model('File', fileSchema);
