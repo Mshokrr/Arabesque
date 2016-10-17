22aa325b53de6d6f7ba8993e19c4b58bfd11894d
Mshokrr <mohamedshokrr@gmail.com>
2016-10-17 23:54:02 +0300

    ready to seed old users

--- app/config/seed.js
+++ app/config/seed.js
@@ -2,34 +2,23 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Project = mongoose.model('Project');
 
-var usersConfig = require('./usersDatabaseConfig.js');
 var users = require('../data/users.json');
 
 module.exports.seedUsers = function(req, res){
-
   for (var i = 0; i < users.length; i++){
-    var user = new User();
-    user.mobileNumber = users[i].mobileNumber;
-    user.setPassword("123456");
-    user.level = users[i].level;
-    user.firstName = users[i].firstName;
-    user.lastName = users[i].lastName;
+    var user = {};
+    //var user = new User();
+    user.mobileNumber = "0" + users[i].mobileNumber.toString();
+    user.level = 1;
+    var nameArray = users[i].name.split(" ");
+    var firstName = nameArray[0];
+    var lastNameArray = nameArray.slice(1, nameArray.length);
+    var lastName = lastNameArray.join(" ");
+    user.firstName = firstName;
+    user.lastName = lastName;
     user.email = users[i].email;
-    user.address = users[i].address;
-    user.university = users[i].university;
-    user.faculty = users[i].faculty;
-    user.academicYear = users[i].academicYear;
-    user.save(function(err){
-      if(err){
-        console.log(err);
-        res.status(500).json(err);
-        return;
-      }
-    });
   }
-  res.send(users);
-}
-
-module.exports.seedProjects = function(req, res){
-
+  res.status(200).json({
+    "message" : "done"
+  });
 }
