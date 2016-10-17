51e75227a7e4abca7670ccd04160ebf9c1ec16c6
Mshokrr <mohamedshokrr@gmail.com>
2016-10-18 02:34:47 +0300

    seed script modified

--- app/config/seed.js
+++ app/config/seed.js
@@ -6,17 +6,27 @@ var users = require('../data/users.json');
 
 module.exports.seedUsers = function(req, res){
   for (var i = 0; i < users.length; i++){
-    var user = {};
-    //var user = new User();
+    var user = new User();
     user.mobileNumber = "0" + users[i].mobileNumber.toString();
     user.level = 1;
+    user.setPassword("123456");
     var nameArray = users[i].name.split(" ");
     var firstName = nameArray[0];
     var lastNameArray = nameArray.slice(1, nameArray.length);
     var lastName = lastNameArray.join(" ");
     user.firstName = firstName;
-    user.lastName = lastName;
+    if(lastName === undefined || lastName === ""){
+      user.lastName = " ";
+    }
+    else{
+      user.lastName = lastName;
+    }
     user.email = users[i].email;
+    user.save(function(err){
+      if(err){
+        console.log(err);
+      }
+    });
   }
   res.status(200).json({
     "message" : "done"
