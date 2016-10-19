74f66f6d75f2463198a20a5cf9b76ce99c12f218
Mshokrr <mohamedshokrr@gmail.com>
2016-10-20 02:44:46 +0300

    sign in by enter key and limiting input academic year

--- public/core.js
+++ public/core.js
@@ -149,3 +149,16 @@ app.directive("galleryComponent", function(){
 			controller: "galleryCtrl"
 		}
 });
+
+app.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown", function(e) {
+            if(e.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter, {'e': e});
+                });
+                e.preventDefault();
+            }
+        });
+    };
+});
